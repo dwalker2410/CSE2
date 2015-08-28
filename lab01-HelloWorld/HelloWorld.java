76662a137112b2ec078f411d9d1e10984285ec65
dwalker2410 <dkw219@lehigh.edu>
2015-08-28 16:18:20 +0000

    updated HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,23 @@
+///   ///   ///   ///   ///   ///   ///   ///   ///
+// Donterrius K. Walker (aka D.T.; aka D-wall)
+// Wassup World, How are you doing? Java Program
+// 
+// define a class
+public class HelloWorld{
+
+    //add main domain
+        public static void main(String[] args) {
+            
+//   print the statement with box around it
+    System.out.println("########################");
+    System.out.println("########################");
+    System.out.println("#                      #");
+    System.out.println("#   Hello World 2014!  #");
+    System.out.println("#                      #");
+    System.out.println("########################");
+    System.out.println("########################");
+    System.out.println("test");
+    
+    //test/testtest
+    }
+}
