593c1bada5ec9ce05ce3c51851653ffd531aaf09
dwalker2410 <dkw219@lehigh.edu>
2015-08-30 23:23:58 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,20 @@
+// define a class
+public class WelcomeClass{
+
+    //add main domain
+        public static void main(String[] args) {
+
+//   print the statement with box around it
+System.out.println( "  -----------");
+System.out.println( "  | WELCOME |");
+System.out.println( "  -----------");
+System.out.println(   "  ^  ^  ^  ^  ^  ^");
+System.out.println(  " / \\/ \\/ \\/ \\/ \\/ \\");
+System.out.println("<-D--K--W--2--1--9->");
+System.out.println(  " \\ /\\ /\\ /\\ /\\ /\\ /");
+System.out.println(   "  v  v  v  v  v  v");
+System.out.println("On September 7th, I will be officialy 18 years of age. I am thankful for that"); 
+System.out.println("because not a lot of people live long enough to see that. Thank you Lord!");
+
+}
+}
